b7469953f38b3571ea79ca53f807f98f6754dfa5
Başar Yıldırım <110203080+Basar00@users.noreply.github.com>
2026-07-21 23:57:39 +0300

    prv_user_code.c

--- Proje/2026-07-21/prv_user_code.c
+++ Proje/2026-07-21/prv_user_code.c
@@ -4,20 +4,19 @@ void PRV_USER_Code(void)
 {
     uint8_t led_on = 0;
 
-    // 1. Donanım ve Buton başlangıç ayarları
-    HAL_PCU_SetOutputValue(PCU_ID_B, PCU_PIN_ID_4, PCU_PORT_HIGH);   // Ana LED Sönük (PB4)
-    HAL_PCU_SetOutputValue(PCU_ID_B, PCU_PIN_ID_11, PCU_PORT_HIGH);  // 2. LED Sönük (PB11)
+    HAL_PCU_SetOutputValue(PCU_ID_B, PCU_PIN_ID_4, PCU_PORT_HIGH); 
+    HAL_PCU_SetOutputValue(PCU_ID_B, PCU_PIN_ID_11, PCU_PORT_HIGH);
     Button_Init();
 
     while (1)
     {
-        // 2. Buton modülünden güncel olayı (event) al
+        // event al
         Button_Event_e olay = Button_Process();
 
-        // 3. Main sadece event'e tepki verir (Hocanın istediği yapı)
+        //event'e tepki
         if (olay == BUTTON_EVENT_SHORT)
         {
-            // Kısa Basış: Ana LED (PB4) Toggle (Aç / Kapa)
+            // Kısa Basış
             led_on = !led_on;
             if (led_on == 1) {
                 HAL_PCU_SetOutputValue(PCU_ID_B, PCU_PIN_ID_4, PCU_PORT_LOW);
@@ -27,11 +26,13 @@ void PRV_USER_Code(void)
         }
         else if (olay == BUTTON_EVENT_LONG)
         {
-            // Uzun Basış: 2. LED (PB11) üzerinden farklı bir pattern / flaş patlat
+            // Uzun Basış: flaş
             for(int i = 0; i < 3; i++) {
                 HAL_PCU_SetOutputValue(PCU_ID_B, PCU_PIN_ID_11, PCU_PORT_LOW);
+                HAL_PCU_SetOutputValue(PCU_ID_B, PCU_PIN_ID_10, PCU_PORT_LOW);
                 for (volatile uint32_t d = 0; d < 2000; d++);
                 HAL_PCU_SetOutputValue(PCU_ID_B, PCU_PIN_ID_11, PCU_PORT_HIGH);
+                HAL_PCU_SetOutputValue(PCU_ID_B, PCU_PIN_ID_10, PCU_PORT_HIGH);
                 for (volatile uint32_t d = 0; d < 2000; d++);
             }
         }
