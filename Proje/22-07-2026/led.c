b0e768301f9fe8be0399a7126f68721b7db0e16c
Başar Yıldırım <110203080+Basar00@users.noreply.github.com>
2026-07-22 13:47:48 +0300

    Add files via upload

---
+++ Proje/22-07-2026/led.c
@@ -0,0 +1,18 @@
+#include "led.h"
+
+void LED_Init(void) {
+    // 3 LED'i de Çıkış (Output) yap
+    HAL_PCU_SetInOutMode(LED1_PORT, LED1_PIN, PCU_INOUT_OUTPUT_PUSH_PULL);
+    HAL_PCU_SetInOutMode(LED2_PORT, LED2_PIN, PCU_INOUT_OUTPUT_PUSH_PULL);
+    HAL_PCU_SetInOutMode(LED3_PORT, LED3_PIN, PCU_INOUT_OUTPUT_PUSH_PULL);
+
+    // Başlangıçta hepsini söndür
+    LED_UpdateChase(99);
+}
+
+void LED_UpdateChase(uint8_t active_index) {
+    // Sadece sırası gelen LED'i LOW(0) yapıp yakar, diğerlerini HIGH(1) yapıp söndürür
+    HAL_PCU_SetOutputValue(LED1_PORT, LED1_PIN, (active_index == 0) ? PCU_PORT_LOW : PCU_PORT_HIGH);
+    HAL_PCU_SetOutputValue(LED2_PORT, LED2_PIN, (active_index == 1) ? PCU_PORT_LOW : PCU_PORT_HIGH);
+    HAL_PCU_SetOutputValue(LED3_PORT, LED3_PIN, (active_index == 2) ? PCU_PORT_LOW : PCU_PORT_HIGH);
+}
